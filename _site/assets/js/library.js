5c17a97b8a140f28c94cdb8011b6d4a3b2a2dedc
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2019-06-13 10:30:22 +0100

    Cleaned up JS

--- _site/assets/js/library.js
+++ _site/assets/js/library.js
@@ -1,30 +1,69 @@
 window.onload = function(){
-  var checkboxes = document.querySelectorAll(".task-list-item-checkbox");
-  checkboxes.forEach(function(checkbox, i){
-    checkbox.onchange = function(){
-      calculator(checkboxes)
-      if (this.checked) cookie.create("checkbox-"+i)
+    if ( window.location.pathname.indexOf("/PRODUCTIONISATION.html") !== -1) {
+        document.getElementsByClassName("progress_bar")[0].style.display = "block";
+
+        let checkboxes = document.querySelectorAll(".task-list-item-checkbox");
+        checkboxes.forEach(function(checkbox, i){
+            checkbox.onchange = function(){
+                calculator(checkboxes);
+                if (this.checked) cookie.create("checkbox-"+i)
+            };
+            checkbox.disabled = false;
+            checkbox.checked = !!cookie.read("checkbox-"+i);
+        });
+        calculator(checkboxes)
     }
-    checkbox.disabled = false;
-    checkbox.checked = cookie.read("checkbox-"+i)? true:false;
-  });
-  calculator(checkboxes)
-}
+};
 
-var calculator = function(checkboxes){
-  var checked = 0;
-  checkboxes.forEach(function(checkbox){
-    if (checkbox.checked) {
-      checked++;
-    };
-  });
+let calculator = function(checkboxes){
+    let checked = 0;
+    checkboxes.forEach(function(checkbox){
+        if (checkbox.checked) {
+            checked++;
+        }
+    });
 
-  percentageDisplayer((checked/checkboxes.length) * 100)
-}
+    percentageDisplayer((checked/checkboxes.length))
+};
 
-var percentageDisplayer = function(percentage){
-  document.getElementById("percentage_holder").innerHTML = Math.round(percentage)
-}
+let percentageDisplayer = function(percentage){
+    document.getElementById("percentage_holder").innerHTML = Math.round(percentage * 100).toString();
+
+    let colour_green = '#439a63';
+    let colour_red = '#F44336';
+    let colour_yellow = '#FFEB3B';
+
+    let slice_1_deg = percentage * 360;
+
+    let slice_1 = document.getElementById("slice_1");
+    let slice_2 = document.getElementById("slice_2");
+
+    switch (Math.floor(percentage * 3)) {
+        case 0:
+            slice_1.style.background = colour_red;
+            slice_2.style.background = colour_red;
+            break;
+
+        case 1:
+            slice_1.style.background = colour_yellow;
+            slice_2.style.background = colour_yellow;
+            break;
+
+        default:
+            slice_1.style.background = colour_green;
+            slice_2.style.background = colour_green;
+            break;
+    }
+
+    if (slice_1_deg > 180) {
+        let slice_2_deg = slice_1_deg - 180;
+        slice_2.style.transform = "rotate("+slice_2_deg+"deg)";
+        slice_1.style.transform = "rotate(180deg)";
+    } else {
+        slice_2.style.transform = "rotate(0deg)";
+        slice_1.style.transform = "rotate("+slice_1_deg+"deg)";
+    }
+};
 
 
 
@@ -50,30 +89,30 @@ var percentageDisplayer = function(percentage){
 
 
 
-var cookie = {
-  create: function (name, value, days) {
-      var expires;
-      if (days) {
-          var date = new Date();
-          date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-          expires = "; expires=" + date.toUTCString();
-      }
-      else expires = "";
-      document.cookie = name + "=" + value + expires + "; path=/";
-  },
+let cookie = {
+    create: function (name, value, days) {
+        let expires;
+        if (days) {
+            let date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        else expires = "";
+        document.cookie = name + "=" + value + expires + "; path=/";
+    },
 
-  read: function (name) {
-      var nameEQ = name + "=";
-      var ca = document.cookie.split(';');
-      for (var i = 0; i < ca.length; i++) {
-          var c = ca[i];
-          while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-          if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-      }
-      return null;
-  },
+    read: function (name) {
+        let nameEQ = name + "=";
+        let ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    },
 
-  erase: function (name) {
-      cookie.create(name, "", -1);
-  }
+    erase: function (name) {
+        cookie.create(name, "", -1);
+    }
 };

5ac7cf35f5a2e285298c49286bfef6cc50a18551
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2019-06-13 15:05:00 +0100

    Minor bug fixes and tidying up
    Scrolling boldning
    
    Colour change of progress bar
    
    Mobile optimisations

--- assets/js/library.js
+++ assets/js/library.js
@@ -6,12 +6,50 @@ window.onload = function(){
         checkboxes.forEach(function(checkbox, i){
             checkbox.onchange = function(){
                 calculator(checkboxes);
-                if (this.checked) cookie.create("checkbox-"+i)
+                if (this.checked) cookie.create("checkbox-"+i);
+                else cookie.erase("checkbox-"+i);
             };
             checkbox.disabled = false;
             checkbox.checked = !!cookie.read("checkbox-"+i);
         });
         calculator(checkboxes);
+    }
+};
+
+window.onscroll = function() {
+    let scrolltags = document.getElementsByTagName("a");
+
+    let pass = false;
+    let previous = false;
+
+    //Go through each a-tag
+    for(let i=0; i<scrolltags.length; i++) {
+        let a_tag = scrolltags[i];
+
+        //If it's an anchor link
+        if(a_tag.href.indexOf("#")!==-1) {
+            //Get the anchor target
+            let target_id = a_tag.href.split("#")[1];
+            let a_target = document.getElementById(target_id);
+
+            //Get position of that target
+            let position = a_target.getBoundingClientRect();
+
+            if(previous!==false) {
+                //If we haven't scrolled passed it, and nothing is found
+                if (position["y"] > 20 && !pass) {
+                    //Make bold and block further iterations
+                    previous.style.fontWeight = "600";
+                    pass = true;
+                }
+
+                //Otherwise, reset font weight
+                else previous.style.fontWeight = "";
+            }
+
+            //Save this element for future usage
+            previous = a_tag;
+        }
     }
 };
 
@@ -31,10 +69,11 @@ let percentageDisplayer = function(percentage){
 
     let colour_green = '#439a63';
     let colour_red = '#F44336';
-    let colour_yellow = '#FFEB3B';
+    let colour_yellow = '#DFCB0B';
 
     let slice_1_deg = percentage * 360;
 
+    let title = document.getElementsByClassName("progress_bar")[0].getElementsByTagName("h5")[0];
     let slice_1 = document.getElementById("slice_1");
     let slice_2 = document.getElementById("slice_2");
 
@@ -42,16 +81,19 @@ let percentageDisplayer = function(percentage){
         case 0:
             slice_1.style.background = colour_red;
             slice_2.style.background = colour_red;
+            title.style.color = colour_red;
             break;
 
         case 1:
             slice_1.style.background = colour_yellow;
             slice_2.style.background = colour_yellow;
+            title.style.color = colour_yellow;
             break;
 
         default:
             slice_1.style.background = colour_green;
             slice_2.style.background = colour_green;
+            title.style.color = colour_green;
             break;
     }
 
